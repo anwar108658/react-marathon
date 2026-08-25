2121c87dcb18423fcda64254742494d66315813a
anwar108658 <anwar108658@gmail.com>
2026-08-25 12:07:25 +0500

    add currency proj

--- src/currencyExchange_5/Currency.tsx
+++ src/currencyExchange_5/Currency.tsx
@@ -6,16 +6,16 @@ const Currency = () => {
     const [amount,setAmount] = useState(300)
     const [from,setFrom] = useState("usd")
     const [to,setTo] = useState("pkr")
-    const [convertedAmount,setConvertedAmount] = useState("")
+    const [convertedAmount,setConvertedAmount] = useState(0)
 
-    const currencyInfo = useCurrencyInfo(from)
+    const currencyInfo: Record<string, number> = useCurrencyInfo(from)
     console.log(currencyInfo,"info")
 
-    const options = Object.keys(currencyInfo) || []
+    const options = Object.keys(currencyInfo) || {}
 
     const swap = () => {
       setFrom(to)
-      setTo(from )
+      setTo(from)
     }
     const convert = () => {
       setConvertedAmount(amount * currencyInfo[to])
@@ -31,29 +31,36 @@ const Currency = () => {
                 <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                     <form
                         onSubmit={(e) => {
-                            e.preventDefault();
-                           
+                          e.preventDefault();
+                          convert()
                         }}
                     >
                         <div className="w-full mb-1">
                             <Input
                               label="From"
-                              
+                              amount={amount}
+                              currencyOpt={options}
+                              onCurrencyChange={(currency:any) => setAmount(currency)}
+                              onAmountChange={(amount:any) => setAmount(amount)}
+                              selectCurrency={from}
                             />
                         </div>
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                                
+                                onClick={swap}
                             >
                                 swap
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
                             <Input
-                                label="To"
-                                
+                              label="To"
+                              amount={convertedAmount}
+                              currencyOpt={options}
+                              onCurrencyChange={(currency:any) => setTo(currency)}
+                              selectCurrency={to}
                             />
                         </div>
                         <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
--- src/currencyExchange_5/components/Input.tsx
+++ src/currencyExchange_5/components/Input.tsx
@@ -1,6 +1,7 @@
 
 
 const Input = ({label,amount,onAmountChange,onCurrencyChange,currencyOpt=[],selectCurrency="usd",amountDsiable=false,currencyDisable,className = "",}:any) => {
+    console.log("first",currencyOpt)
   return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
@@ -9,7 +10,7 @@ const Input = ({label,amount,onAmountChange,onCurrencyChange,currencyOpt=[],sele
                 </label>
                 <input
                     
-                    className="outline-none w-full bg-transparent py-1.5"
+                    className="outline-none text-black w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
                     disabled={amountDsiable}
@@ -20,9 +21,9 @@ const Input = ({label,amount,onAmountChange,onCurrencyChange,currencyOpt=[],sele
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    className="rounded-lg px-1 py-1 bg-gray-900 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={(e) => onCurrencyChange && onCurrencyChange(parseInt(e.target.value))}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
                     
